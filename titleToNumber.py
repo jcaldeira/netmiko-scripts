957cebfba9e40178dfb8d41e5598bc49827c4846
JCaldeira <50929436+jcaldeira@users.noreply.github.com>
2021-02-17 19:23:54 +0000

    Re-create titleToNumber.py

---
+++ titleToNumber.py
@@ -0,0 +1,34 @@
+'''Driver function
+print(titleToNumber("CDA"));
+
+This code contributed by Rajput-Ji
+https://www.geeksforgeeks.org/find-excel-column-number-column-title/
+
+Python program to return title to result
+of excel sheet.'''
+
+import sys
+
+# Returns resul when we pass title.
+def titleToNumber(s):
+    # This process is similar to binary-to-
+    # decimal conversion
+    result = 0;
+    for B in range(len(s)):
+        result *= 26;
+        result += ord(s[B]) - ord('A') + 1;
+
+    return result;
+
+
+def main():
+    user_input = input('Name of the column: ')
+    while True:
+        if user_input != 'exit':
+            print(titleToNumber(user_input))
+        else:
+            sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
